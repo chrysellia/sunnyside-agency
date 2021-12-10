152dfd29a08cd45349a4417dd60af0d96261d4a0
Chrysellia <priscilliachryso@gmail.com>
2021-12-10 11:27:39 +0300

    Navbar and Hero component created

---
+++ sunnyside-agency/src/Components/Hero/Hero.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import'./Hero.style.scss';
+
+const Hero = () => {
+    return (
+        <div className="hero-component">
+            <div className="hero-image" style={{backgroundImage:'url(images/desktop/image-header.jpg)'}}></div>
+        </div>
+    )
+}
+
+export default Hero
--- sunnyside-agency/src/Components/Navbar/Navbar.component.js
+++ sunnyside-agency/src/Components/Navbar/Navbar.component.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.style.scss';
+// import Hero from '../Hero/Hero.component';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="navbar-component">
+            <div className="navbar-image" style={{backgroundImage:'url(images/desktop/image-header.jpg)'}}>
+                {/* <Hero/> */}
+                <div className="arrow-icon">
+                    <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF" stroke-width="6" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M18 3v100M3 95.484l15 15 15-15"/></g>
+                    </svg>
+                </div>
+                <div className="navbar-menu">
+                    <div className="navbar-logo">sunnyside</div>
+                    <div className="list-menu">
+                        <div className="list-item">About</div>
+                        <div className="list-item">Services</div>
+                        <div className="list-item">Projects</div>
+                        <div className="list-item-active">CONTACT</div>
+                    </div>
+                </div>
+                <div className="navbar-title">WE ARE CREATIVES</div>
+            </div>
         </div>
     )
 }
