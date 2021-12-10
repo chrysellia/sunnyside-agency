0e36c2da53c606874b85cc78629193445c078971
Chrysellia <priscilliachryso@gmail.com>
2021-12-10 10:32:01 +0300

    Start project sunnyside-agency

---
+++ sunnyside-agency/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import HomePage from './Components/HomePage/HomePage.component';
+
+const App = () => {
+    return(
+      <HomePage/>
+    )
+}
+
+export default App;
