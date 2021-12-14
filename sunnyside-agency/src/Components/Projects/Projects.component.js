d1b780e28cff3ce02e4766fa543e60f4857ced66
Chrysellia <priscilliachryso@gmail.com>
2021-12-14 10:48:57 +0300

    Projects component mobile version

--- sunnyside-agency/src/Components/Projects/Projects.component.js
+++ sunnyside-agency/src/Components/Projects/Projects.component.js
@@ -3,20 +3,23 @@ import './Projects.style.scss';
 
 const Projects = () => {
     return (
-
         <div className="projects-component">
-            <div className="projects-image">
-                <div className="image-item">
-                    <img src="images/desktop/image-gallery-milkbottles.jpg"/>
+            <div className="desktop-side">
+                <div className="projects-image">
+                    <div className="image-item">
+                        <img src="images/desktop/image-gallery-milkbottles.jpg"/>
+                    </div>
+                    <div className="image-item">
+                        <img src="images/desktop/image-gallery-orange.jpg"/>
+                    </div>
                 </div>
-                <div className="image-item">
-                    <img src="images/desktop/image-gallery-orange.jpg"/>
-                </div>
-                <div className="image-item">
-                    <img src="images/desktop/image-gallery-cone.jpg"/>
-                </div>
-                <div className="image-item">
-                    <img src="images/desktop/image-gallery-sugarcubes.jpg"/>
+                <div className="projects-image">
+                    <div className="image-item">
+                        <img src="images/desktop/image-gallery-cone.jpg"/>
+                    </div>
+                    <div className="image-item">
+                        <img src="images/desktop/image-gallery-sugarcubes.jpg"/>
+                    </div>
                 </div>
             </div>
         </div>
